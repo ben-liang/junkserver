87f846207c2b8aeb99e45e4b0cc699e5a43a7975
Ben Liang <bliang@cliquesads.com>
2016-05-22 23:09:26 -0400

    Geo template & routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET Routes for geo-located sites */
+router.get('/en-eu', function(req, res, next) {
+  res.render('geo-specific-index', { title: 'EU Site' });
+});
+
+router.get('/en-us', function(req, res, next) {
+  res.render('geo-specific-index', { title: 'US Site' });
+});
+
+router.get('/en-au', function(req, res, next) {
+  res.render('geo-specific-index', { title: 'AU Site' });
+});
+
 module.exports = router;
