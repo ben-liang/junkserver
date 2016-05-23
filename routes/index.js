986e5787afdce4ee003c9318c0bdc9036a11fdb1
Ben Liang <bliang@cliquesads.com>
2016-05-23 12:06:29 -0400

    Change to accomodate subdomains

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/*', function(req, res, next) {
+  res.render('index', { title: 'Hey Brad or Ben!', 'hostname': req.hostname, 'path': req.path });
 });
 
 /* GET Routes for geo-located sites */
-router.get('/en-eu', function(req, res, next) {
-  res.render('geo-specific-index', { title: 'EU Site' });
-});
-
-router.get('/en-us', function(req, res, next) {
-  res.render('geo-specific-index', { title: 'US Site' });
-});
-
-router.get('/en-au', function(req, res, next) {
-  res.render('geo-specific-index', { title: 'AU Site' });
-});
+// router.get('/en-eu', function(req, res, next) {
+//     res.render('geo-specific-index', { title: 'EU Site' });
+// });
+//
+// router.get('/en-us', function(req, res, next) {
+//     res.render('geo-specific-index', { title: 'US Site' });
+// });
+//
+// router.get('/en-au', function(req, res, next) {
+//     res.render('geo-specific-index', { title: 'AU Site' });
+// });
 
 module.exports = router;
